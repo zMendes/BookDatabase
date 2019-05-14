e384877f09c2164d890d33db20c03e581f2c5af9
Leonardo <leonardom1808@gmail.com>
2019-05-14 18:45:58 -0300

    Homepage design

---
+++ app/src/main/java/br/edu/insper/al/leonardomm4/bookdatabase/Homepage.java
@@ -0,0 +1,13 @@
+package br.edu.insper.al.leonardomm4.bookdatabase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
