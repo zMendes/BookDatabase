7157352e5bf9022c41452e584a620f854b47d921
Leonardo <leonardom1808@gmail.com>
2019-05-19 13:45:03 -0300

    OnClickListener primeiro botão

--- app/src/main/java/br/edu/insper/al/leonardomm4/bookdatabase/Homepage.java
+++ app/src/main/java/br/edu/insper/al/leonardomm4/bookdatabase/Homepage.java
@@ -1,13 +1,23 @@
 package br.edu.insper.al.leonardomm4.bookdatabase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Homepage extends AppCompatActivity {
+
+    private Button book1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        book1 = findViewById(R.id.book1);
+
+        book1.setOnClickListener(view -> {
+            Intent intent = new Intent(this, BookPage.class);
+            startActivity(intent);
+        });
     }
 }
